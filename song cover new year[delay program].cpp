f32a4139fe13652acc29e2f3ce8f71df630f636e
Andika Putra Wijaya <128115929+DikaWasHere@users.noreply.github.com>
2023-03-19 20:28:01 +0700

    Add files via upload

---
+++ song cover new year[delay program].cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+
+
+
+void delay(int a){
+	int add,time,i;
+	
+	time= a*100000000;
+	for(i=0;i<time;i++){
+		add*=i;
+		add++;
+		add++;
+	}
+}
+
+
+int main(){
+	string a,b,c;
+	
+		cout<<" Rasa ini  "<<endl;
+		delay(1);
+	
+		cout<<endl;
+		cout<<"     Terukir jelas "<<endl;
+		delay(3.1);
+		cout<<endl;
+		
+			cout<<"         Dalam Dadaku "<<endl;
+		delay(5.2);
+		system("cls");
+		
+	system("COLOR 1");
+	cout<<" karena mu "<<endl;
+	delay(5.7);
+	system("cls");
+	
+	
+	cout<<"karna dirimu "<<endl;
+     delay(6.2);
+     system("cls");
+
+	cout<<"telah mengajarkanku... "<<endl;
+	delay(5.8);
+	system("cls");
+
+     cout<<"Dalam kelam langit malampun "<<endl;
+	delay(6.2);
+	system("cls");
+	
+	cout<<"Bintang kan bersinar penuh "<<endl;
+	delay(6.5);
+	system("cls");
+	
+	cout<<"Jangan kau sembunyikan pilu "<<endl;
+	delay(6.8);
+	system("cls");
+	
+	cout<<"Dibalik senyum sendumu "<<endl;
+	delay(7.3);
+	system("cls");
+	
+	cout<<"Semua gemerlap cahya itu "<<endl;
+	delay(7.4);
+	system("cls");
+	
+	cout<<"Tertuju pada dirimu "<<endl;
+	delay(7.4);
+	system("cls");
+
+}
